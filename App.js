27d8349c7e54153653e6e00eb03746f91d7d0222
Dabin Jang <djang@dhcp-wifi-8021x-168-122-223-20.bu.edu>
2021-09-17 14:41:16 -0400

    fix some errors

--- App.js
+++ App.js
@@ -48,24 +48,6 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 import apiKeys from './config/keys';
 
-export async function registration(email, password, lastName, firstName) {
-  try {
-    await firebase.auth().createUserWithEmailAndPassword(email, password);
-    const currentUser = firebase.auth().currentUser;
-
-    const db = firebase.firestore();
-    db.collection("users")
-      .doc(currentUser.uid)
-      .set({
-        email: currentUser.email,
-        lastName: lastName,
-        firstName: firstName,
-      });
-  } catch (err) {
-    Alert.alert("There is something wrong!", err.message);
-  }
-}
-
 //screens
 import CameraScreen from "./camera";
 import IngredientScreen from "./IngredientScreen";
